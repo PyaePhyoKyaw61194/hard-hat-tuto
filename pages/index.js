439a39c9379f5b899954f3909b8ade4a58b5acd5
Joe <pyaephyokyaw.work@gmail.com>
2024-07-09 16:11:27 +0700

    fix: migrate to version 6

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import CounterContract from "./../artifacts/contracts/Counter.sol/Counter.json";
 import { ethers } from "ethers";
 
@@ -6,76 +6,71 @@ const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const abi = CounterContract.abi;
 
 export default function App() {
-    const [connectButton, setConnectButton] = useState("Connect")
-    const [count, setCount] = useState()
+    const [connectButton, setConnectButton] = useState("Connect");
+    const [count, setCount] = useState(null);
     const [contract, setContract] = useState(null);
 
     useEffect(() => {
-        if (typeof window.ethereum !== "undefined") {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const contractInstance = new ethers.Contract(contractAddress, abi, signer);
-            setContract(contractInstance);
-        } else {
-            console.error("Ethereum object doesn't exist!");
-        }
+        const initializeContract = async () => {
+            if (typeof window.ethereum !== "undefined") {
+                const provider = new ethers.BrowserProvider(window.ethereum);
+                const signer = await provider.getSigner();
+                const contractInstance = new ethers.Contract(contractAddress, abi, signer);
+                setContract(contractInstance);
+            } else {
+                console.error("Ethereum object doesn't exist!");
+            }
+        };
+
+        initializeContract();
     }, []);
 
     const handleConnect = async () => {
-
         if (typeof window.ethereum !== 'undefined') {
             await window.ethereum.request({ method: 'eth_requestAccounts' });
-            setConnectButton("Connected")
+            setConnectButton("Connected");
         } else {
-            setConnectButton("Please install MetaMask")
+            setConnectButton("Please install MetaMask");
         }
-    }
+    };
 
     const getCount = async () => {
         try {
             const data = await contract.get();
-            setCount(data.toNumber());
-
+            setCount(data.toString());
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-
-    }
+    };
 
     const handleInc = async () => {
         try {
             const tx = await contract.inc();
             await tx.wait();
-            await getCount()
+            await getCount();
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-
-    }
+    };
 
     const handleDec = async () => {
-
         try {
             const tx = await contract.dec();
             await tx.wait();
-            await getCount()
+            await getCount();
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-
-    }
-
+    };
 
     return (
         <div>
             <button onClick={handleConnect}>{connectButton}</button>
             <button onClick={getCount}>Get Count</button>
-            <button onClick={handleInc}>Inc </button>
-            <button onClick={handleDec}>Dec </button>
-
+            <button onClick={handleInc}>Inc</button>
+            <button onClick={handleDec}>Dec</button>
             <hr />
             <h1>Count : {count}</h1>
-
         </div>
-    )
+    );
 }
