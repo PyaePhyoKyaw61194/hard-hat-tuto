ee22d0c44a324ed0fcccd52d05fa3707f6ae5719
Joe <pyaephyokyaw.work@gmail.com>
2024-07-09 14:37:39 +0700

    feat: get count from FE

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import CounterContract from "./../artifacts/contracts/Counter.sol/Counter.json";
+import { ethers } from "ethers";
+
+const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const abi = CounterContract.abi;
+
 export default function App() {
     const [connectButton, setConnectButton] = useState("Connect")
 
@@ -11,9 +17,28 @@ export default function App() {
             setConnectButton("Please install MetaMask")
         }
     }
+
+    const getCount = async () => {
+        if (typeof window.ethereum !== "undefined") {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, abi, signer);
+
+            try {
+                const data = await contract.get();
+                console.log("data: ", data.toNumber());
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
+
+
     return (
         <div>
             <button onClick={handleConnect}>{connectButton}</button>
+            <button onClick={getCount}>Get Count</button>
+
         </div>
     )
 }
