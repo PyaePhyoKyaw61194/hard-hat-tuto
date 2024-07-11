0924670bfaefec73200f58a562e0c286681c46b2
Joe <pyaephyokyaw.work@gmail.com>
2024-07-11 14:05:31 +0700

    feat: deploy to sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   defaultNetwork: "hardhat",
   networks: {
-  /*   hardhat:{
-      accounts:[
-        {
-          privateKey: "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
-          balance: "100000000000000000000000"
-        }
-      ],
-      chainId: 31337
-    } */
+    /*   hardhat:{
+        accounts:[
+          {
+            privateKey: "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+            balance: "100000000000000000000000"
+          }
+        ],
+        chainId: 31337
+      } */
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOILA_PRIVATE_KEY],
+      chainId: 11155111,
+    }
   }
 };
